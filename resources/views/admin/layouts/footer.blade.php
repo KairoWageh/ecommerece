e90e4a10f47634f17c49e421f49155ba83bd8d95
KairoWageh <kairo.wageh@gmail.com>
2020-08-21 23:49:29 +0200

    Direct root

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -19,23 +19,23 @@
     });
     </script>
     <!--dataTables-->
-    <script src="{{asset('design/adminpanel/js/dataTables.bootstrap.min.css')}}"></script>
-    <script src="{{asset('design/adminpanel/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('design/adminpanel/js/dataTables.bootstrap.min.js')}}"></script>
+    <script src="{{asset('public/design/adminpanel/js/dataTables.bootstrap.min.css')}}"></script>
+    <script src="{{asset('public/design/adminpanel/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('public/design/adminpanel/js/dataTables.bootstrap.min.js')}}"></script>
     <!--//dataTables-->
     <!--scrolling js-->
-	  <script src="{{asset('design/adminpanel/js/jquery.nicescroll.js')}}"></script>
-	  <script src="{{asset('design/adminpanel/js/scripts.js')}}"></script>
+	  <script src="{{asset('public/design/adminpanel/js/jquery.nicescroll.js')}}"></script>
+	  <script src="{{asset('public/design/adminpanel/js/scripts.js')}}"></script>
 		<!--//scrolling js-->
-    <script src="{{asset('design/adminpanel/js/bootstrap.js')}}"> </script>
-    <script src="{{asset('design/adminpanel/js/dataTables.buttons.min.js')}}"> </script>
+    <script src="{{asset('public/design/adminpanel/js/bootstrap.js')}}"> </script>
+    <script src="{{asset('public/design/adminpanel/js/dataTables.buttons.min.js')}}"> </script>
     <script src="{{asset('vendor/datatables/buttons.server-side.js')}}"> </script>
-    <script src="{{asset('design\adminpanel\jstree\jstree.js')}}"></script>
-    <script src="{{asset('design\adminpanel\jstree\jstree.checkbox.js')}}"></script>
-    <script src="{{asset('design\adminpanel\jstree\jstree.wholerow.js')}}"></script>
+    <script src="{{asset('public/design\adminpanel\jstree\jstree.js')}}"></script>
+    <script src="{{asset('public/design\adminpanel\jstree\jstree.checkbox.js')}}"></script>
+    <script src="{{asset('public/design\adminpanel\jstree\jstree.wholerow.js')}}"></script>
     <!--datepicker js-->
-    <script src="{{asset('design/adminpanel/js/bootstrap-datepicker.min.js')}}"></script>
-    <script src="{{asset('design/adminpanel/js/bootstrap-datepicker.ar.js')}}"></script>
+    <script src="{{asset('public/design/adminpanel/js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('public/design/adminpanel/js/bootstrap-datepicker.ar.js')}}"></script>
     <!--//datepicker js-->
 
   <!-- mother grid end here-->
