3c68ccc8e57da0556852560293d1a2548994c8e7
KairoWageh <kairo.wageh@gmail.com>
2020-08-21 23:46:19 +0200

    Direct root

--- resources/views/admin/forgot_password.blade.php
+++ resources/views/admin/forgot_password.blade.php
@@ -1,8 +1,3 @@
-<!--Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -13,13 +8,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<link href="{{asset('design/adminpanel/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
+<link href="{{asset('public/design/adminpanel/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
 <!-- Custom Theme files -->
-<link href="{{asset('design/adminpanel/css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
+<link href="{{asset('public/design/adminpanel/css/style.css')}}" rel="stylesheet" type="text/css" media="all"/>
 <!--js-->
-<script src="{{asset('design/adminpanel/js/jquery-2.1.1.min.js')}}"></script> 
+<script src="{{asset('public/design/adminpanel/js/jquery-2.1.1.min.js')}}"></script> 
 <!--icons-css-->
-<link href="{{asset('design/adminpanel/css/font-awesome.css')}}" rel="stylesheet"> 
+<link href="{{asset('public/design/adminpanel/css/font-awesome.css')}}" rel="stylesheet"> 
 <!--Google Fonts-->
 <link href='//fonts.googleapis.com/css?family=Carrois+Gothic' rel='stylesheet' type='text/css'>
 <link href='//fonts.googleapis.com/css?family=Work+Sans:400,500,600' rel='stylesheet' type='text/css'>
@@ -54,10 +49,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <!--COPY rights end here-->
 
 <!--scrolling js-->
-		<script src="{{asset('design/adminpanel/js/jquery.nicescroll.js')}}"></script>
-		<script src="{{asset('design/adminpanel/js/scripts.js')}}"></script>
+		<script src="{{asset('public/design/adminpanel/js/jquery.nicescroll.js')}}"></script>
+		<script src="{{asset('public/design/adminpanel/js/scripts.js')}}"></script>
 		<!--//scrolling js-->
-<script src="{{asset('design/adminpanel/js/bootstrap.js')}}"> </script>
+<script src="{{asset('public/design/adminpanel/js/bootstrap.js')}}"> </script>
 <!-- mother grid end here-->
 </body>
 </html>
