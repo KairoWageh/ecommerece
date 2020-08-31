987a26fa87d5a07703e1c53f465271187c4d675a
KairoWageh <kairo.wageh@gmail.com>
2020-08-31 20:12:08 +0200

    User routes

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+// access User folder in controller  ===> using namespace User
+Route::group(['prefix' => '', 'namespace' => 'User'], function(){
+	Route::get('/', function () {
+	    return view('site.home');
+	});
+	// user:web ====> middleware:guard
+	Route::group(['middleware' => 'user:web'], function(){
+		Route::get('/categories', 'DepartmentsController@index')->name('user.categories');	
+	});
+});
+
 Route::group(['middleware' => 'maintenance'], function(){
 	Route::get('/', function () {
 	    return view('site.home');
 	});
 });
+
 Route::get('maintenance', function(){
 	if(setting()->status == 'open'){
 		return redirect('/');
