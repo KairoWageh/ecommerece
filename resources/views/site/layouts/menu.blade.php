7b618dab673f6317840bd68e7eaabeeb7e8c6641
KairoWageh <kairo.wageh@gmail.com>
2020-09-02 09:19:11 +0200

    Add categories link

--- resources/views/site/layouts/menu.blade.php
+++ resources/views/site/layouts/menu.blade.php
@@ -16,7 +16,7 @@
                     <li><a href="single-product.html">Single product</a></li>
                     <li><a href="cart.html">Cart</a></li>
                     <li><a href="checkout.html">Checkout</a></li>
-                    <li><a href="#">Category</a></li>
+                    <li><a href="{{ route('user.categories') }}">Category</a></li>
                     <li><a href="#">Others</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
