f5621f0906dac494fc5c351a251eb306a07dd274
KairoWageh <kairo.wageh@gmail.com>
2020-09-02 15:12:21 +0200

    add user language file

---
+++ resources/lang/ar/user.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	"login"                            => "تسجيل الدخول",
+    "logout"                           => "تسجيل الخروج",
+    "language"                         => "اللغة",
+    "email"                            => "البريد الإلكتروني",
+    "password"                         => "كلمة المرور",
+    "wrong_creditionals"               => "البريد الإلكتروني أو كلمة المرور غير صحيحة.",
+    "forgot_password"                  => "هل نسيت كلمة المرور؟",
+    "my_account"                       => "حسابي",
+    "wishlist"                         => "Wishlist",
+    "my_cart"                          => "بطاقتي",
+    "checkout"                         => "Checkout",
+    "home"                             => "الرئيسية",
+    "shop_page"                        => "Shop page",
+    "departments"                       => "الأقسام",
+    
+];
