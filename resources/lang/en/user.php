915ccd9a9d3703560b0b82dedc87e08a1dbdd538
KairoWageh <kairo.wageh@gmail.com>
2020-09-02 15:13:58 +0200

    add user language file

---
+++ resources/lang/en/user.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+	"login"                            => "Login",
+    "logout"                           => "Logout",
+    "language"                         => "Language",
+    "email"                            => "Email",
+    "password"                         => "Password",
+    "wrong_creditionals"               => "Email or password is incorrect.",
+    "forgot_password"                  => "Forgot password?",
+    "my_account"                       => "My Account",
+    "wishlist"                         => "Wishlist",
+    "my_cart"                          => "My Cart",
+    "checkout"                         => "Checkout",
+    "home"                             => "Home",
+    "shop_page"                        => "Shop page",
+    "department"                       => "Department",
+    
+
+];
