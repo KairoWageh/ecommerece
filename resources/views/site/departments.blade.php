533e96460d7625232a24bb18706936c363c50433
KairoWageh <kairo.wageh@gmail.com>
2020-09-03 11:10:15 +0200

    update departments

--- resources/views/site/departments.blade.php
+++ resources/views/site/departments.blade.php
@@ -1,26 +1,19 @@
 @extends('site.index')
 
 @section('content')
-
-@foreach($departments as $department)
-	<div class="single-product">
-        <div class="product-f-image">
-            <img src="{{ Storage::url($department->icon) }}" alt="department_icon" width="300" height="300">
+<div class="container">
+    @foreach($departments as $department)
+    	<div class="col-md-4">
+            <div class="product-f-image" style="height: 50%; width: 50%">
+                <img src="{{ Storage::url($department->icon) }}" alt="department_icon">
+            </div>
+            @if(app()->getLocale() == 'ar')
+            	<h4><a href="departments/{{$department->department_name_ar}}" style="text-decoration: none;"> {{$department->department_name_ar}}</a></h4>
+            @else
+            	<h4><a href="departments/{{$department->department_name_en}}" style="text-decoration: none;"> {{$department->department_name_en}}</a></h4>
+            @endif                               
         </div>
-        @if(app()->getLocale() == 'ar')
-        	<h2>{{$department->department_name_ar}}</h2>
-        @else
-        	<h2>{{$department->department_name_en}}</h2>
-        @endif
-
-        <div class="product-carousel-price">
-            <ins>$400.00</ins> <del>$425.00</del>
-        </div>                                 
-    </div>
-	<div class="clear-fix"></div>
-	<!-- <h1>
-		{{$department->department_name_ar}}
-	</h1>	 -->
-@endforeach
-
+    	<div class="clear-fix"></div>
+    @endforeach
+</div>
 @endsection
