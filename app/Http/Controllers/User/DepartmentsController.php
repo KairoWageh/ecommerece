620151e2afb7af236b6a1b1cf75786340afcba5f
KairoWageh <kairo.wageh@gmail.com>
2020-09-03 11:14:42 +0200

    update DepartmentsController

--- app/Http/Controllers/User/DepartmentsController.php
+++ app/Http/Controllers/User/DepartmentsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Department;
+use App\Product;
 
 class DepartmentsController extends Controller
 {
@@ -12,5 +13,12 @@ class DepartmentsController extends Controller
     {
         $departments = Department::select('*')->whereNotIn('status', [-1])->get();
         return view('site.departments', compact('departments'));
+    }
+
+    public function single_dep($dep_name){
+    	$department = Department::where('department_name_ar', $dep_name)->orWhere('department_name_en', $dep_name)->get();
+    	$department_id = $department[0]->id;
+    	$products = Product::select('*')->where('department_id', $department_id)->whereNotIn('status', [-1])->get();
+    	return view('site.single_dep', compact('products'));
     }
 }
