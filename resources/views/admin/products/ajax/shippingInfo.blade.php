1b9602ed9606115bc4220abb307895daaa97d661
KairoWageh <kairo.wageh@gmail.com>
2020-09-05 09:43:37 +0200

    Add files via upload

---
+++ resources/views/admin/products/ajax/shippingInfo.blade.php
@@ -0,0 +1,34 @@
+<div class="col-md-6">
+	<div class="form-group">
+	    {!! Form::label('sizes', __('admin.size_id'), ['class' => 'col-md-3']) !!}
+	    <div class="col-md-9">
+	    	{!! Form::select('sizes',  $sizes,  ['class' => 'form-control', 'placeholder' => __('admin.size_id')]) !!}	
+	    </div>
+	</div>
+
+	<div class="form-group">
+	    {!! Form::label('size', __('admin.size'), ['class' => 'col-md-3']) !!}
+	    <div class="col-md-9">
+	    	{!! Form::text('size', '',  ['class' => 'form-control', 'placeholder' => __('admin.size')]) !!}	
+	    </div>
+	</div>
+</div>
+
+
+
+<div class="col-md-6">
+	<div class="form-group">
+	    {!! Form::label('weight_id', __('admin.weight_id'), ['class' => 'col-md-3']) !!}
+	    <div class="col-md-9">
+	    	{!! Form::select('weight_id',  $weights,  ['class' => 'form-control', 'placeholder' => __('admin.weight_id')]) !!}	
+	    </div>
+	</div>
+
+	<div class="form-group">
+	    {!! Form::label('weight', __('admin.weight'), ['class' => 'col-md-3']) !!}
+	    <div class="col-md-9">
+	    	{!! Form::text('weight', '',  ['class' => 'form-control', 'placeholder' => __('admin.weight')]) !!}	
+	    </div>
+	</div>
+</div>
+<div class="clearfix"></div>
