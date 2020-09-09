3cade698a6ed54d3cdc15d0b9aa0bdb4b2affe1b
KairoWageh <kairo.wageh@gmail.com>
2020-09-09 09:21:41 +0200

    update other_data file

--- resources/views/admin/products/tabs/other_data.blade.php
+++ resources/views/admin/products/tabs/other_data.blade.php
@@ -38,19 +38,21 @@
 <div id="other_data" class="tab-pane fade">
 	<h3>{{__('admin.other_data')}}</h3>
 	<div class="div_inputs col-sm-12">
+		@foreach($product->other_data()->get() as $other_data)
 		<div>
 			<div class="col-sm-6">
 				{!! Form::label('input_key', __('admin.input_key')) !!}
-				{!! Form::text('input_key[]', '', ['class' => 'form-control']) !!}
+				{!! Form::text('input_key[]', $other_data->data_key, ['class' => 'form-control']) !!}
 			</div>
 			<div class="col-sm-6">
 				{!! Form::label('input_value', __('admin.input_value')) !!}
-				{!! Form::text('input_value[]', '', ['class' => 'form-control']) !!}
+				{!! Form::text('input_value[]', $other_data->data_value, ['class' => 'form-control']) !!}
 			</div>
 			<div class="clearfix"></div>
 			<br>
 			<a href="#" class="remove_input btn btn-danger"><i class="fa fa-trash"></i></a>
 		</div>
+		@endforeach
 	</div>
 	<div class="clearfix"></div>
 	<br>
