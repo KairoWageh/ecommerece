e4806a5b9041e7769d310c0bf422b30617ce2f87
KairoWageh <kairo.wageh@gmail.com>
2020-09-09 09:21:08 +0200

    update product_size_weight file

--- resources/views/admin/products/tabs/product_size_weight.blade.php
+++ resources/views/admin/products/tabs/product_size_weight.blade.php
@@ -1,7 +1,25 @@
 @push('js')
 <script type="text/javascript">
 	$(document).ready(function () {
-		$('.mall_select2').select2();
+		var dataSelect = [
+			@foreach(App\Country::all() as $country)
+			{
+				"text": "{{ $country->{'country_name_'.lang()} }}",
+				"children":[
+					@foreach($country->malls()->get() as $mall)
+					{
+						"id": "{{ $mall->id }}",
+						"text" : "{{ $mall->{'name_'.lang()} }}",
+						@if(check_mall($product->id, $mall->id))
+						"selected" : true
+						@endif
+					},
+					@endforeach
+				],
+			},
+			@endforeach
+		];
+		$('.mall_select2').select2({data: dataSelect});
 	});
 </script>
 @endpush
@@ -14,27 +32,27 @@
 	<div class="info_data hidden col-md-12">
 		<div class="form-group col-sm-4 col-xs-12">
 		    {!! Form::label('color_id', __('admin.color')) !!}
-			{!! Form::select('color', App\Color::pluck('name_'.lang(), 'id'), $product->color_id, ['class' => 'form-control', 'placeholder' => __('admin.color')]) !!}
+			{!! Form::select('color_id', App\Color::pluck('name_'.lang(), 'id'), $product->color_id, ['class' => 'form-control', 'placeholder' => __('admin.color')]) !!}
 		</div>
 		<div class="form-group col-sm-4 col-xs-12">
 		    {!! Form::label('trade_mark_id', __('admin.trademark')) !!}
-			{!! Form::select('trademark', App\TradeMark::pluck('name_'.lang(), 'id'), $product->trade_mark_id, ['class' => 'form-control', 'placeholder' => __('admin.trademark')]) !!}
+			{!! Form::select('trade_mark_id', App\TradeMark::pluck('name_'.lang(), 'id'), $product->trade_mark_id, ['class' => 'form-control', 'placeholder' => __('admin.trademark')]) !!}
 		</div>
 		<div class="form-group col-sm-4 col-xs-12">
 		    {!! Form::label('manu_id', __('admin.manufacture')) !!}
-			{!! Form::select('manufacture', App\Manufacturer::pluck('name_'.lang(), 'id'), $product->manu_id, ['class' => 'form-control', 'placeholder' => __('admin.manufacture')]) !!}
+			{!! Form::select('manu_id', App\Manufacturer::pluck('name_'.lang(), 'id'), $product->manu_id, ['class' => 'form-control', 'placeholder' => __('admin.manufacture')]) !!}
 		</div>
 		<div class="clearfix"></div>
 		<div class="col-sm-12">
 		    {!! Form::label('malls', __('admin.malls')) !!}
 			<select name="mall[]" class="form-control mall_select2" multiple="multiple" style="width:100%">
-				@foreach(App\Country::all() as $country)
+				<!-- @foreach(App\Country::all() as $country)
 					<optgroup label="{{ $country->{'country_name_'.lang()} }}">
 						@foreach($country->malls()->get() as $mall)
 							<option value="{{ $mall->id }}">{{ $mall->{'name_'.lang()} }}</option>
 						@endforeach
 					</optgroup>
-				@endforeach
+				@endforeach -->
 			</select>
 		</div>
 		<div class="clearfix"></div>
