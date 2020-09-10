ac0cc8ebd50248f03f478966c162b2ce420efa30
KairoWageh <kairo.wageh@gmail.com>
2020-09-10 09:10:46 +0200

    apdate auth language file

--- resources/lang/ar/auth.php
+++ resources/lang/ar/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'هذه السجلات غير صحيحة.',
+    'throttle' => 'محاولات تسجيل دخول كثيرة.. يرجى المجاولة مرة أخرى خلال  :seconds ثانية.',
 
 ];
