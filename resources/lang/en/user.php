ee0000c55ecf420685d3f77d9429b21991d393d5
KairoWageh <kairo.wageh@gmail.com>
2020-10-18 15:05:01 +0200

    Update user.php

--- resources/lang/en/user.php
+++ resources/lang/en/user.php
@@ -18,6 +18,38 @@ return [
     "add_to_cart"                      => "Add to cart",
     "see_datails"                      => "See datails",
     "latest_products"                  => "Latest Products",
+
+
+    // cart page
+    "cart"                             => "Cart",
+    "shopping_cart"                    => "Shopping Cart",
+    "search_products"                  => "Search Products",
+    "product"                          => "Product",
+    "price"                            => "Price",
+    "quantity"                         => "Quantity",
+    "total"                            => "Total",
+    "cart_empty"                       => "Your shopping cart is empty.",
+
+
+    "first_name"                       => "First name",
+    "last_name"                        => "Last name",
+    "address"                          => "Address",
+    "city"                             => "City",
+    "country"                          => "Country",
+    "post_code"                        => "Post code",
+    "phone_number"                     => "Phone number",
+    "other_notes"                      => "Other notes",
+    "never_share"                      => "We'll never share your email with anyone else.",
+
+
+
+
+
+    // footer
+    "all_rights_reserved"              => "All rights reserved.",
+    "design_by"                        => "Design by",
+
+
     
 
 ];
