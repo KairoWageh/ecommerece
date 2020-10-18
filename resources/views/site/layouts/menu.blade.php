290e36a9cc8ec08760c1f98d8236bc7b7d5a585f
KairoWageh <kairo.wageh@gmail.com>
2020-10-18 14:51:57 +0200

    Update menu.blade.php

--- resources/views/site/layouts/menu.blade.php
+++ resources/views/site/layouts/menu.blade.php
@@ -9,14 +9,22 @@
                     <span class="icon-bar"></span>
                 </button>
             </div> 
+            <?php 
+                $directoryURI = $_SERVER['REQUEST_URI'];
+                $path = parse_url($directoryURI, PHP_URL_PATH);
+                $components = explode('/', $path);
+                $first_part = $components[2];
+            ?>
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="{{ url('/') }}">{{__('user.home')}}</a></li>
+                    <li class="<?php if ($first_part=="") {echo "active"; }?>"><a href="{{ url('/') }}">{{__('user.home')}}</a></li>
                     <li><a href="shop.html">Shop page</a></li>
-                    <li><a href="single-product.html">Single product</a></li>
-                    <li><a href="cart.html">Cart</a></li>
+
+                    <li class="<?php if ($first_part=="cart") {echo "active"; }?>">
+                        <a href="{{ url('/cart') }}" >{{__('user.cart')}}</a>
+                    </li>
                     <li><a href="checkout.html">Checkout</a></li>
-                    <li><a href="{{ route('user.departments') }}">{{__('user.departments')}}</a></li>
+                    <li class="<?php if ($first_part=="departments") {echo "active"; }?>"><a href="{{ route('user.departments') }}">{{__('user.departments')}}</a></li>
                     <li><a href="#">Others</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
