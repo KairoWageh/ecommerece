b9dfbf5c4229e4571baa6cb33b1140ca50b80da8
KairoWageh <kairo.wageh@gmail.com>
2020-10-18 15:03:40 +0200

    Update HomeController.php

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Product;
+use App\Manufacturer;
 
 class HomeController extends Controller
 {
     public function home(){
     	$latest_products = Product::latest('id')->where('status', 1)->where('product_status', 'active')->limit(6)->get();
+    	$manufactures = Manufacturer::all()->where('status', 1);
+
     	// if(count($latest_products) == 0){
     	// 	$latest_products = 
     	// }
-    	return view('site.home', compact('latest_products'));
+    	return view('site.home', compact('latest_products', 'manufactures'));
     }
 }
