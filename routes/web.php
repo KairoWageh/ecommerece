34639dcbf669a1800a10b9eeffab26e466aca30b
KairoWageh <kairo.wageh@gmail.com>
2020-10-19 17:18:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,30 +23,52 @@ Route::group(['prefix' => '', 'namespace' => 'User'], function(){
 
 
 	 // change language of site using url
-	Route::get('lang/{lang}', function($lang){
+	Route::get('/lang/{lang}', function($lang){
+		//return session('lang');
 		session()->has('lang')? session()->forget('lang'): '';
 		$lang == 'ar'? session()->put('lang', 'ar'): session()->put('lang', 'en');
 		return back();
-	});
+	})->name('lang');
 
 	//if and only if user is authenticated
 	// user:web ====> middleware:guard
 	Route::group(['middleware' => 'user:web'], function(){
 
-		Route::get('/', function () {
-		    return view('site.home');
-		});
+		Route::get('/', 'HomeController@home');
+		Route::get('account', 'AccountController@index')->name('account');
+		Route::get('shop', 'ShopController@index')->name('shop');
+		Route::get('product/{product}', 'ProductsController@single_product')->name('user.single_product');
 		Route::get('/departments', 'DepartmentsController@index')->name('user.departments');
-		Route::get('/departments/{dep_name}', 'DepartmentsController@single_dep')->name('user.single_dep');	
+		Route::get('/departments/{dep_name}', 'DepartmentsController@single_dep')->name('user.single_dep');
+
+
+		// cart routes
+		Route::get('/cart', 'CartController@getCart')->name('cart');
+		Route::get('/cart/item/{id}/add', 'CartController@addToCart')->name('cart.add');
+		Route::get('/cart/item/{id}/remove', 'CartController@removeFromCart')->name('cart.remove');
+		Route::get('/cart/clear', 'CartController@clearCart')->name('cart.clear');	
+		// Route::post('/cart/checkOut', 'CartController@checkOut')->name('cart.checkOut');
+		Route::get('/cart/checkOut', 'CartController@checkOut')->name('cart.checkOut');
+		Route::post('/cart/pay', 'CartController@pay')->name('cart.pay');
+		// Route::get('checkout/payment/complete', 'Site\CheckoutController@complete')->name('checkout.payment.complete');
+		Route::get('cart/payment/complete', 'CartController@complete')->name('cart.payment.complete');
+
+		Route::get('account/orders', 'Site\AccountController@getOrders')->name('account.orders');
+
+
+		// Route::get('/plans', 'PlanController@index')->name('plans.index');
+		// Route::get('/plan/{plan}', 'PlanController@show')->name('plans.show');
+
+		// Route::post('/subscription', 'SubscriptionController@create')->name('subscription.create');
 	});
 
 
 });
 
+
+// un logged in users can see home page 
 Route::group(['middleware' => 'maintenance'], function(){
-	Route::get('/', function () {
-	    return view('site.home');
-	});
+	Route::get('/', 'User\HomeController@home');
 });
 
 Route::get('maintenance', function(){
